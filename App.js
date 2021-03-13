16a5d1b0734597fd804ce521c418986cdd5126bb
matheusgir <girardi.matheus@hotmail.com>
2021-03-13 19:34:24 -0300

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import Routes from './src/routes'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar hidden={true}/>
+      <Routes />
+    </NavigationContainer>
   );
 }
 
